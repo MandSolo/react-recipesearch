fa8638e0e5a7379c2c095da6386d1d8b9fc83ac3
MandSolo <mandcashin@hotmail.co.uk>
2019-01-29 21:45:44 +0000

    tested getRecipe with console.log

--- src/App.js
+++ src/App.js
@@ -3,13 +3,21 @@ import Form from "./components/Form.js";
 import "./App.css";
 
 class App extends Component {
+
+getRecipe = (e) => {
+e.preventDefault();
+console.log("hello")
+
+}
+
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">Recipe Search</h1>
         </header>
-        <Form />
+        <Form getRecipe={this.getRecipe} />
       </div>
     );
   }
